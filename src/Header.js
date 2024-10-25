00f6a357dbf2d2eafcf773de9cc2c70158a16f35
Oshank Piplani <93657837+oshankpiplani@users.noreply.github.com>
2024-10-26 00:58:19 +0530

    oauthChanges

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import create from "./images/icons8-create-50.png";
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import Auth from "./auth/auth"
 export default function Header() {
   return (
     <div className="header-logo">
@@ -13,6 +14,9 @@ export default function Header() {
           alt="Create new post"
         />
       </Link>
+      <GoogleOAuthProvider clientId="896811511013-3iq3nd2jmgjdrk6f0gisqpg6ajdetb6b.apps.googleusercontent.com">
+          <Auth></Auth>
+      </GoogleOAuthProvider>
     </div>
   );
 }
